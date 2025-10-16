99d83ec3730cddb857a09a3a4dc0b5bc1a9e7aca
Sumra Yasmin <sumrayasmin9@gmail.com>
2025-10-16 23:03:56 +0500

    Configure Solid Cable for real-time message broadcasting on task pages.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,17 @@
 class MessagesController < ApplicationController
+  before_action :set_task
+
   def create
-    @task = Task.find(params[:task_id])
     @message = @task.messages.build(message_params)
-    
-    if @message.save
-      redirect_to @task, notice: "Message was successfully posted."
-    else
-      redirect_to @task, alert: "Failed to post message."
-    end
+
+    @message.save
   end
 
   private
+
+  def set_task
+    @task = Task.find(params[:task_id])
+  end
 
   def message_params
     params.require(:message).permit(:content)
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,15 @@
 class Message < ApplicationRecord
   belongs_to :task
+  after_create_commit :broadcast_message
+
+  private
+
+  def broadcast_message
+    broadcast_append_to(
+      [task, 'messages'],
+      target: 'messages',
+      partial: 'messages/message',
+      locals: { message: self }
+    )
+  end
 end
